c8caf0f940c9c286f0eee5811453a2b6a2984636
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2023-08-25 14:40:04 -0700

    🔨 Add proper error handling to `from_str`
    The `Digest::from_str` function was checked-in in a
    partially-implemented state that only everaged [`Option`] values; though
    this is not ideal since it can't convey the actual failure reason.
    
    This reworks the implementation to have a proper `ParseDigestError`
    object that may be returned that indicates the underlying error cause.

--- core/src/crypto/sha256.rs
+++ core/src/crypto/sha256.rs
@@ -10,6 +10,39 @@
 #[repr(align(16))]
 pub struct Digest([u8; 32]);
 
+#[derive(Clone, Copy)]
+enum DigestErrorKind {
+  BadChar(char),
+  BadLength(usize),
+}
+
+/// An error raised when attempting to convert a [`str`] into a [`Digest`].
+#[derive(Clone, Copy)]
+pub struct ParseDigestError(DigestErrorKind);
+
+impl core::fmt::Display for ParseDigestError {
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+    match &self.0 {
+      DigestErrorKind::BadChar(ch) => write!(
+        f,
+        "bad character in digest string; '{}' is not hexadecimal",
+        ch
+      ),
+      DigestErrorKind::BadLength(len) => write!(
+        f,
+        "bad length of digest string; expected 64-chars, found {}",
+        len
+      ),
+    }
+  }
+}
+
+impl core::fmt::Debug for ParseDigestError {
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+    <Self as core::fmt::Display>::fmt(&self, f)
+  }
+}
+
 impl Digest {
   /// Constructs a [`Digest`] containing only zero values.
   #[inline(always)]
@@ -52,25 +85,64 @@ impl Digest {
       let c0 = bytes[i * 2];
       let c1 = bytes[i * 2 + 1];
 
-      result.0[i] = Self::hex_digit_to_u8(c0) << 4 | Self::hex_digit_to_u8(c1);
+      result.0[i] = Self::hex_digit_to_u8_unchecked(c0) << 4
+        | Self::hex_digit_to_u8_unchecked(c1);
     }
     result
   }
 
-  pub fn from_str(s: &str) -> Option<Self> {
+  /// Constructs this Digest from a string representation of the digest,
+  /// with error checking.
+  ///
+  /// Returns a [`ParseDigestError`] on failure if the supplied string either
+  /// does not contain 64 characters, or if any of the digits are not valid
+  /// hexadecimal values.
+  ///
+  /// # Arguments
+  ///
+  /// * `s` - the string to parse
+  ///
+  /// # Examples
+  ///
+  /// Basic usage:
+  ///
+  /// ```rust
+  /// # use core::crypto::sha256::Digest;
+  /// let sha = "invalid"
+  /// let digest = Digest::from_str(sha);
+  ///
+  /// assert!(sha.is_err());
+  /// ```
+  pub fn from_str(s: &str) -> Result<Self, ParseDigestError> {
     if s.len() != 64 {
-      return None;
+      return Err(ParseDigestError(DigestErrorKind::BadLength(s.len())));
     }
-    Some(Self::zeroed())
+    let mut result = Self::zeroed();
+    let bytes = s.as_bytes();
+    for i in 0..32 {
+      let c0 = bytes[i * 2];
+      let c1 = bytes[i * 2 + 1];
+
+      result.0[i] =
+        Self::hex_digit_to_u8(c0)? << 4 | Self::hex_digit_to_u8(c1)?;
+    }
+
+    Ok(result)
   }
 
-  /// Converts an 8-bit ascii hexadecimal value into its correspondign integer
-  /// form.
+  /// Converts an 8-bit ascii hexadecimal value into its corresponding integer
+  /// form without checking.
   ///
   /// # Arguments
   ///
   /// * `ascii` - the 8-bit ascii value
-  unsafe fn hex_digit_to_u8(ascii: u8) -> u8 {
+  ///
+  /// # Safety
+  ///
+  /// This function is unsafe because it assumes that the input values are
+  /// valid ASCII characters -- and anything outside this range may corrupt the
+  /// computation.
+  unsafe fn hex_digit_to_u8_unchecked(ascii: u8) -> u8 {
     let ch = char::from(ascii);
     match ch {
       '0'..='9' => {
@@ -88,11 +160,45 @@ impl Digest {
     }
   }
 
+  /// Converts an 8-bit ascii hexadecimal value into its corresponding integer
+  /// form.
+  ///
+  /// This function returns a [`ParseDigestError`] on failure.
+  ///
+  /// # Arguments
+  ///
+  /// * `ascii` - the 8-bit ascii value
+  fn hex_digit_to_u8(ascii: u8) -> Result<u8, ParseDigestError> {
+    let ch = char::from(ascii);
+    match ch {
+      '0'..='9' => {
+        let ord: u32 = ch.into();
+        let zero: u32 = '0'.into();
+        Ok((ord - zero) as u8)
+      }
+      'a' | 'A' => Ok(10u8),
+      'b' | 'B' => Ok(11u8),
+      'c' | 'C' => Ok(12u8),
+      'd' | 'D' => Ok(13u8),
+      'e' | 'E' => Ok(14u8),
+      'f' | 'F' => Ok(15u8),
+      _ => Err(ParseDigestError(DigestErrorKind::BadChar(ch))),
+    }
+  }
+
   /// Returns an iterator over the bytes within the digest.
   ///
   /// The iterator yields all items from start to end.
   pub fn iter(&self) -> impl Iterator<Item = &u8> {
     self.0.iter()
+  }
+
+  /// Creates a consuming iterator, that is, one that moves each value out of
+  /// the digest (from start to end). Since each value is a [`u8`] which
+  /// satisfies [`Copy`], this mostly exists for APIs that expect values
+  /// rather than references.
+  pub fn into_iter(self) -> impl IntoIterator<Item = u8> {
+    self.0.into_iter()
   }
 }
 
@@ -105,31 +211,48 @@ impl core::fmt::Display for Digest {
   }
 }
 
-#[cfg(test)]
 impl core::fmt::Debug for Digest {
   fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
     <Self as core::fmt::Display>::fmt(self, f)
   }
 }
 
-/// A 64-byte Block of data that may be
+/// A 64-byte Block of data that is hashed in the SHA256 algorithm.
+///
+/// This is largely a thin-wrapper of an array of 64-bytes, with added alignment
+/// to help along code-generation so that it may leverage better registers or
+/// loading calls.
+///
+/// Block objects can deref directly into slices of [`u8`] for convenience.
 #[derive(Clone)]
 #[repr(align(32))]
 pub struct Block([u8; 64]);
 
 impl Block {
+  /// The size of all [`Block`] instances.
   pub const SIZE: usize = 64;
 
+  /// Constructs a [`Block`] containing only zeros.
   #[inline]
   pub const fn zeroed() -> Self {
     Self([0; 64])
+  }
+
+  /// Constructs a [`Block`] from an array of the same size.
+  ///
+  /// # Arguments
+  ///
+  /// * `value` - the array value to use.
+  #[inline(always)]
+  pub const fn from_array(value: [u8; 64]) -> Self {
+    Self(value)
   }
 }
 
 impl From<[u8; 64]> for Block {
   #[inline(always)]
   fn from(value: [u8; 64]) -> Self {
-    Self(value)
+    Self::from_array(value)
   }
 }
 
