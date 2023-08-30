067692e80989fa9d37d1567346d1d8d4b212b0c4
Matthew Rodusek <7519129+bitwizeshift@users.noreply.github.com>
2023-08-30 13:26:41 -0400

    🔨 Reuse `sha256::Digest` for other crypto ops
    The `sha256::Digest` provides a lot of useful, sharable utilities that
    could otherwise be used by digests for other crypto operations. Rather
    than rewriting all of this logic later, the digest has instead been
    made generic on the byte-size, renamed to `FixedDigest`, and moved to
    the top-level `crypto` module.

--- core/src/crypto.rs
+++ core/src/crypto.rs
@@ -123,3 +123,198 @@ impl<T: Hashable> Hashable for [T] {
     }
   }
 }
+
+/// A 32-byte SHA256 digest, which contains the state of a SHA256 hash
+/// operation.
+///
+/// This type is aligned to a 16-byte boundary so that the compiler may take
+/// advantage of this for better code-generation.
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+#[repr(align(16))]
+pub struct FixedDigest<const N: usize>([u8; N]);
+
+impl<const N: usize> FixedDigest<N> {
+  /// Constructs a [`Digest`] containing only zero values.
+  #[inline(always)]
+  const fn zeroed() -> Self {
+    Self([0; N])
+  }
+
+  /// Constructs this Digest from a string representation of the digest,
+  /// without doing any error checking on the input.
+  ///
+  /// # Arguments
+  ///
+  /// * `s` - the string to parse
+  ///
+  /// # Safety
+  ///
+  /// This function is unsafe because it does not check that the string passed
+  /// to it form a valid hex Digest. If the input string `s` is not a 2 * N
+  /// character ascii hexadecimal string, this will cause memory unsafety
+  /// issues such as possible out-of-bounds access or buffer overflow issues.
+  ///
+  /// Ensure that the input string is valid before using, or prefer the
+  /// [`Digest::from_str`] instead.
+  ///
+  /// # Examples
+  ///
+  /// Basic usage:
+  ///
+  /// ```rust
+  /// # use core::crypto::FixedDigest;
+  /// let digest = unsafe {
+  ///   // Digest for "Hello, world!"
+  ///   FixedDigest::<32>::from_str_unchecked("315f5bdb76d078c43b8ac0064e4a0164612b1fce77c869345bfc94c75894edd3")
+  /// };
+  /// ```
+  pub unsafe fn from_str_unchecked(s: &str) -> Self {
+    let mut result = Self::zeroed();
+    let bytes = s.as_bytes();
+    for i in 0..N {
+      let c0 = bytes[i * 2];
+      let c1 = bytes[i * 2 + 1];
+
+      result.0[i] = Self::hex_digit_to_u8_unchecked(c0) << 4
+        | Self::hex_digit_to_u8_unchecked(c1);
+    }
+    result
+  }
+
+  /// Constructs this Digest from a string representation of the digest,
+  /// with error checking.
+  ///
+  /// Returns a [`ParseDigestError`] on failure if the supplied string either
+  /// does not contain 64 characters, or if any of the digits are not valid
+  /// hexadecimal values.
+  ///
+  /// # Arguments
+  ///
+  /// * `s` - the string to parse
+  ///
+  /// # Examples
+  ///
+  /// Basic usage:
+  ///
+  /// ```rust
+  /// # use core::crypto::FixedDigest;
+  /// let sha = "invalid"
+  /// let digest = FixedDigest::<32>::from_str(sha);
+  ///
+  /// assert!(sha.is_err());
+  /// ```
+  pub fn from_str(s: &str) -> Result<Self, ParseDigestError> {
+    if s.len() != 2 * N {
+      return Err(ParseDigestError(DigestErrorKind::BadLength(s.len())));
+    }
+    let mut result = Self::zeroed();
+    let bytes = s.as_bytes();
+    for i in 0..N {
+      let c0 = bytes[i * 2];
+      let c1 = bytes[i * 2 + 1];
+
+      result.0[i] =
+        Self::hex_digit_to_u8(c0)? << 4 | Self::hex_digit_to_u8(c1)?;
+    }
+
+    Ok(result)
+  }
+
+  /// Converts an 8-bit ascii hexadecimal value into its corresponding integer
+  /// form without checking.
+  ///
+  /// # Arguments
+  ///
+  /// * `ascii` - the 8-bit ascii value
+  ///
+  /// # Safety
+  ///
+  /// This function is unsafe because it assumes that the input values are
+  /// valid ASCII characters -- and anything outside this range may corrupt the
+  /// computation.
+  unsafe fn hex_digit_to_u8_unchecked(ascii: u8) -> u8 {
+    let ch = char::from(ascii);
+    match ch {
+      '0'..='9' => {
+        let ord: u32 = ch.into();
+        let zero: u32 = '0'.into();
+        (ord - zero) as u8
+      }
+      'a' | 'A' => 10u8,
+      'b' | 'B' => 11u8,
+      'c' | 'C' => 12u8,
+      'd' | 'D' => 13u8,
+      'e' | 'E' => 14u8,
+      'f' | 'F' => 15u8,
+      _ => unreachable!(),
+    }
+  }
+
+  /// Converts an 8-bit ascii hexadecimal value into its corresponding integer
+  /// form.
+  ///
+  /// This function returns a [`ParseDigestError`] on failure.
+  ///
+  /// # Arguments
+  ///
+  /// * `ascii` - the 8-bit ascii value
+  fn hex_digit_to_u8(ascii: u8) -> Result<u8, ParseDigestError> {
+    let ch = char::from(ascii);
+    match ch {
+      '0'..='9' => {
+        let ord: u32 = ch.into();
+        let zero: u32 = '0'.into();
+        Ok((ord - zero) as u8)
+      }
+      'a' | 'A' => Ok(10u8),
+      'b' | 'B' => Ok(11u8),
+      'c' | 'C' => Ok(12u8),
+      'd' | 'D' => Ok(13u8),
+      'e' | 'E' => Ok(14u8),
+      'f' | 'F' => Ok(15u8),
+      _ => Err(ParseDigestError(DigestErrorKind::BadChar(ch))),
+    }
+  }
+
+  /// Returns an iterator over the bytes within the digest.
+  ///
+  /// The iterator yields all items from start to end.
+  pub fn iter(&self) -> impl Iterator<Item = &u8> {
+    self.0.iter()
+  }
+
+  /// Creates a consuming iterator, that is, one that moves each value out of
+  /// the digest (from start to end). Since each value is a [`u8`] which
+  /// satisfies [`Copy`], this mostly exists for APIs that expect values
+  /// rather than references.
+  pub fn into_iter(self) -> impl IntoIterator<Item = u8> {
+    self.0.into_iter()
+  }
+}
+
+impl<const N: usize> core::fmt::Display for FixedDigest<N> {
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+    for v in &self.0 {
+      write!(f, "{:02x}", v)?;
+    }
+    Ok(())
+  }
+}
+
+impl<const N: usize> core::fmt::Debug for FixedDigest<N> {
+  fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+    <Self as core::fmt::Display>::fmt(self, f)
+  }
+}
+
+impl<const N: usize> Hashable for FixedDigest<N> {
+  /// Updates the hasher by feeding the bytes of this digest into it.
+  ///
+  /// # Arguments
+  ///
+  /// * `hasher` - the hasher to update
+  #[inline]
+  fn update_hash<H: Hasher>(&self, hasher: &mut H) {
+    hasher.update(&self.0)
+  }
+}
